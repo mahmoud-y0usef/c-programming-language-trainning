9978bb995c8e25ee9cf8b5a1c9ab7e6a8bf6e2a4
Mahmoud Yousef <68308514+mahmoud-y0usef@users.noreply.github.com>
2023-11-05 01:08:45 +0200

    Update printf-ex9.c

--- print f/printf-ex9.c
+++ print f/printf-ex9.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-//Travel Program : One of the hobbies many people like is to travel to different countries and learn about people's cultures. Required: Write a program that prints 5 countries you want to travel to.
+//welcome Travel Program : One of the hobbies many people like is to travel to different countries and learn about people's cultures. Required: Write a program that prints 5 countries you want to travel to.
 int main(void){
     printf("russia\n"
            "spain\n"
